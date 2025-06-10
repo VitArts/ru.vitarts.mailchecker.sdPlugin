387fb492922af98071b48e5f0803d5d9e2d2b3cc
VitArts <vitartts@ya.ru>
2025-06-10 17:38:18 +0300

    fix open url

--- plugin/func/openInBrowser.js
+++ plugin/func/openInBrowser.js
@@ -1,3 +1,5 @@
+const { exec } = require('child_process');
+
 /**
  * Открывает URL в браузере в зависимости от ОС.
  * process.platform может быть 'win32', 'darwin' или другое (Linux).
@@ -5,9 +7,9 @@
 function openInBrowser(url) {
   log.info(`Opening URL: ${url}`);
   try {
-    if (process.platform === 'win32') execSync(`start "" "${url}"`);
-    else if (process.platform === 'darwin') execSync(`open "${url}"`);
-    else execSync(`xdg-open "${url}"`);
+    if (process.platform === 'win32') exec(`start "" "${url}"`);
+    else if (process.platform === 'darwin') exec(`open "${url}"`);
+    else exec(`xdg-open "${url}"`);
   } catch (err) {
     log.error('Cannot open URL:', err);
   }
